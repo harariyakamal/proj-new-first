16cbefbea26ffea36a32fa89acf12b13a843b7c9
Kamal Harariya <harariyakamal@gmail.com>
2022-12-10 21:07:35 -0800

    Some more commits.

--- ToDoList/ToDoList/HomeView/HomeViewController.swift
+++ ToDoList/ToDoList/HomeView/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  ToDoList
 //
 //  Created by Kamal Harariya on 12/10/22.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
