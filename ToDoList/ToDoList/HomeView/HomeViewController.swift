775a9638ed567e82691bee23d4790ffced65826f
Kamal Harariya <harariyakamal@gmail.com>
2022-12-10 21:14:39 -0800

    Added a tableview Set random background Added some cosmetic code.

--- ToDoList/ToDoList/HomeView/HomeViewController.swift
+++ ToDoList/ToDoList/HomeView/HomeViewController.swift
@@ -9,8 +9,22 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCellID", for: indexPath)
+        cell.backgroundColor = .random
+        return cell
     }
 }
 
